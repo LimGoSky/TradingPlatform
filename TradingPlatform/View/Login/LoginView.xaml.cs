3a9d0a33f46f29940570552da78ba570f586ce7b
利民 石 <shi888888@hotmail.com>
2019-04-22 23:24:57 +0800

    添加登录框

---
+++ TradingPlatform/View/Login/LoginView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TradingPlatform.View.Login
+{
+    /// <summary>
+    /// LoginView.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void ColorWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
