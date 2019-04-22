2da1daef873baaa138ec13f98275e018c4113e96
bing hu <hu772188392@163.com>
2019-04-23 00:21:22 +0800

    首页样式修改

--- TradingPlatform/MainWindow.xaml.cs
+++ TradingPlatform/MainWindow.xaml.cs
@@ -25,10 +25,77 @@ namespace TradingPlatform
     {
         public MainWindow()
         {
-            LoginLogic model = new LoginLogic();
-            ResultModel result = model.SendMessage("15620938880", CheckCodeTypeEnum.REGISTER.ToString());
+            //LoginLogic model = new LoginLogic();
+            //ResultModel result = model.SendMessage("15620938880", CheckCodeTypeEnum.REGISTER.ToString());
             //Log4Helper.Info(this.GetType(), "abc");
             InitializeComponent();
         }
+
+        #region 标题栏事件
+
+        /// <summary>
+        /// 窗口移动事件
+        /// </summary>
+        private void TitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        int i = 0;
+        /// <summary>
+        /// 标题栏双击事件
+        /// </summary>
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            i += 1;
+            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+            timer.Tick += (s, e1) => { timer.IsEnabled = false; i = 0; };
+            timer.IsEnabled = true;
+
+            if (i % 2 == 0)
+            {
+                timer.IsEnabled = false;
+                i = 0;
+                this.WindowState = this.WindowState == WindowState.Maximized ?
+                              WindowState.Normal : WindowState.Maximized;
+            }
+        }
+
+        /// <summary>
+        /// 窗口最小化
+        /// </summary>
+        private void btn_min_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized; //设置窗口最小化
+        }
+
+        /// <summary>
+        /// 窗口最大化与还原
+        /// </summary>
+        private void btn_max_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal; //设置窗口还原
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized; //设置窗口最大化
+            }
+        }
+
+        /// <summary>
+        /// 窗口关闭
+        /// </summary>
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion 标题栏事件
     }
 }
