244fe06b2ca6c72fd10a2e9d10d1cb0231c6ef80
bing hu <hu772188392@163.com>
2019-05-08 23:58:23 +0800

    首页

--- TradingPlatform/ViewModel/Quotation/Quotation .cs
+++ TradingPlatform/ViewModel/Quotation/Quotation .cs
@@ -15,41 +15,66 @@ namespace TradingPlatform.ViewModel.Quotation
     }
     public class data
     {
-
-        //合约编号
+        /// <summary>
+        /// 序号
+        /// </summary>
+        public int SerialNumber { get; set; }
+        /// <summary>
+        /// 合约编号
+        /// </summary>
         public string contractCode { get; set; }
 
 
-        //合约名
+        /// <summary>
+        /// 合约名
+        /// </summary>
         public string contractName { get; set; }
-        //买价
+        /// <summary>
+        /// 买价
+        /// </summary>
         public string ask { get; set; }
-        //买量
+        /// <summary>
+        /// 买量
+        /// </summary>
         public string askVol { get; set; }
 
-        //卖价
+        /// <summary>
+        /// 卖价
+        /// </summary>
         public string bid { get; set; }
 
-        //卖量
+        /// <summary>
+        /// 卖量
+        /// </summary>
         public string bidVol { get; set; }
 
-        //最高价
+        /// <summary>
+        /// 最高价
+        /// </summary>
         public string highestPrice { get; set; }
 
 
-        //最新价
+        /// <summary>
+        /// 最新价
+        /// </summary>
         public string latestPrice { get; set; }
 
 
-        //最低价
+        /// <summary>
+        /// 最低价
+        /// </summary>
         public string lowestPrice { get; set; }
 
 
-        //开盘价
+        /// <summary>
+        /// 开盘价
+        /// </summary>
         public string openPrice { get; set; }
 
 
-        //成交量
+        /// <summary>
+        /// 成交量
+        /// </summary>
         public string volume { get; set; }
     }
 }
